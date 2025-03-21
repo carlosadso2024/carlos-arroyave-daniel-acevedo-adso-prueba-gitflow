d8058481b3eeb64c0fb5c7ef0c9aadf152faceb6
carlosadso2024 <carlosmario.a@hotmail.com>
2025-03-21 13:17:56 -0500

    commit inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+class Inventario {
+    metodoA() {
+    console.log("Ejecutando método A");
+    }
+   
+    metodoB() {
+    console.log("Ejecutando método B");
+    }
+   
+    metodoC() {
+    console.log("Ejecutando método C");
+    }
+   
+    metodoD() {
+    console.log("Ejecutando método D");
+    }
+   }
+   
